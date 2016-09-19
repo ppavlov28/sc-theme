6cfece63556a5cef30d6ce4df56c97f9a05cea30
Pavel Pavlov <ppavlov28@gmail.com>
2016-09-19 21:44:44 +0300

    Change pagination template.

--- category.php
+++ category.php
@@ -49,8 +49,16 @@
 
 	</section>
 
-
-
+	<?php
+	add_filter('navigation_markup_template', 'my_navigation_template', 10, 2 );
+	function my_navigation_template( $template, $class ){
+		return '
+	<nav class="navigation %1$s" role="navigation">
+		<div class="nav-links">%3$s</div>
+	</nav>    
+	';
+	}
+	?>
 	<?php the_posts_pagination(); ?>
 
 </section>
