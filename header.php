a077e744a305267f436ad18ad42590ee6d1593dd
Pavel Pavlov <ppavlov28@gmail.com>
2016-10-21 21:40:09 +0300

    Итеграция верстки хедера.

--- header.php
+++ header.php
@@ -13,45 +13,46 @@
 <div class="wrapper">
 	<div class="content-full">
 	<!-- Main Header -->
-	<header class="header-main  container">
+	<header class="header-main  header-main--index  container">
 		<div class="wrap clearfix">
-			<div class="header-logo">
+			<div class="header-main__logo">
 				<a href="/">
 					<img src="<?php echo get_template_directory_uri() . '/img/salmon_logo_new.png' ?>" alt="Logo"
 					     width="64" height="60">
 				</a>
 			</div>
-			<h1 class="header-title">Salmon Content</h1>
+			<h1 class="header-main__title">Salmon Content</h1>
+			<button class="main-menu-toggler  no-js"  id="mainMenuToggler">
+				<span>menu toggler</span>
+			</button>
+			<!-- Languages -->
+			<ul class="lang-list">
+				<li class="active"><a href="">Ru</a></li>
+				<li><a href="">En</a></li>
+			</ul>
+			<!-- end of Languages -->
+			<!-- Main Navigation -->
+			<nav class="main-nav">
+				<?php
+				if (has_nav_menu('top')) {
+					wp_nav_menu(
+						array(
+							'theme_location' => 'top',
+							'container'      => 'ul',
+							'menu_class'     => 'menu-top-menu'
+						)
+					);
+				}
+				?>
+			</nav>
+			<!-- end of Main Navigation -->
 		</div>
 	</header>
 	<!-- end of Main Header -->
 	<!-- Slider -->
 	<?php if (is_front_page()) { ?>
-		<section class="slider container" >
-			<?php echo do_shortcode("[metaslider id=316]");	?>
-			<!--
-			<div class="slider-content" style="background-image: url(<?php //echo get_template_directory_uri()
-			?>/img/slider_placeholder.jpg)">
-			</div >
-			-->
+		<section class="slider  container" >
+			<?php echo do_shortcode("[metaslider id=333]");	?>
 		</section >
 	<?php } ?>
 <!-- end of Slider -->
-<!-- Main Navigation -->
-
-<nav class="container">
-	<?php
-	if (has_nav_menu('top')) {
-		wp_nav_menu(
-			array(
-				'theme_location' => 'top',
-				'container'      => 'ul',
-				'menu_class'     => 'main-nav'
-			)
-		);
-	}
-	?>
-</nav>
-
-
-<!-- end of Main Navigation -->
