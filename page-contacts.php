3423a9c65d91aabdb20351f90f6ff800f7563ec0
Pavel Pavlov <ppavlov28@gmail.com>
2016-11-12 00:36:07 +0300

    SCT-28 Классы для формы обратной связи

--- page-contacts.php
+++ page-contacts.php
@@ -1,21 +1,57 @@
 <?php get_header(); ?>
 
 <section class="main  main--white  clearfix page-catalog">
-	<!-- PageTitle -->
+
+	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+			<!-- PageTitle -->
+			<div class="content-title">
+				<div class="wrap">
+					<h2><?php the_title(); ?></h2>
+				</div>
+			</div>
+			<!-- end of PageTitle -->
+
+			<!-- Articles -->
+			<div class="welcome">
+				<div class="wrap">
+					<?php the_content(); ?>
+				</div>
+			</div>
+			<div class="clearfix">
+				<div class="sale-depart">
+					<div class="wrap">
+						<p>Отдел продаж:</p>
+						<p class="sale-depart__item">
+							<a href="tel:<? the_field('tel'); ?>"><?php the_field('tel') ?></a>
+						</p>
+						<p class="sale-depart__item">
+							<a href="mailto:<?php the_field('email') ?>"><?php the_field('email') ?></a>
+						</p>
+					</div>
+				</div>
+				<?php //echo do_shortcode('[contact-form-7 id="343" title="Contact form"]'); ?>
+			</div>
+
+	<?php endwhile; ?>
+
+
+	<!-- PageTitle
 	<div class="content-title">
 		<div class="wrap">
 			<h1>Наши контакты</h1>
 		</div>
 	</div>
-	<!-- end of PageTitle -->
-	<!-- Welcome -->
+	end of PageTitle -->
+	<!-- Welcome
 	<div class="welcome">
 		<div class="wrap">
 			Будем рады помочь по любым вопросам. Звоните нам, пишите на электронную почту
 			или воспользуйтесь формой обратной связи.
 		</div>
 	</div>
-	<!-- end of Welcome -->
+	end of Welcome -->
+	<!--
 	<div class="clearfix">
 		<div class="sale-depart">
 			<div class="wrap">
@@ -28,7 +64,8 @@
 				</p>
 			</div>
 		</div>
-		<?php echo do_shortcode('[contact-form-7 id="343" title="Contact form 1"]'); ?>
+		-->
+		<?php //echo do_shortcode('[contact-form-7 id="343" title="Contact form"]'); ?>
 		<!--
 		<form action="" class="feedback-form" method="post" name="feedback-form">
 			<div class="wrap">
@@ -45,8 +82,10 @@
 				<input type="submit" value="Отправить">
 			</div>
 		</form>
-		-->
+
 	</div>
+
+	-->
 </section>
 
 <?php get_footer(); ?>
